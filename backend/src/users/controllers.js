b474e6d0f0888440a231a663270b98504d985a66
mohamed hisham mahrous <156300477+Mohamed0Hisham@users.noreply.github.com>
2025-02-20 13:46:41 +0200

    enhance: delete friend controller performance

--- backend/src/users/controllers.js
+++ backend/src/users/controllers.js
@@ -256,32 +256,35 @@ export const addFriend = async (req, res) => {
 			error: error.message,
 		});
 	} finally {
-		session.endSession();
+		await session.endSession();
 	}
 };
 export const deleteFriend = async (req, res) => {
+	const session = await startSession();
 	try {
+		session.startTransaction();
+
 		const { userID, friendID } = req.params;
-		if (
-			validator.isMongoId(userID) == false ||
-			validator.isMongoId(friendID) == false
-		) {
+		if (!validator.isMongoId(userID) || !validator.isMongoId(friendID)) {
 			return res.status(400).json({
 				success: false,
-				error: "invalid credentials",
+				error: "Invalid operation",
 			});
 		}
 
-		const user = await User.findById(userID);
-		if (!user) {
+		const users = await User.find({ _id: { $in: [userID, friendID] } });
+		if (users.length !== 2) {
 			return res.status(400).json({
 				success: false,
-				error: "user not found",
+				error: "Invalid user or friend ID",
 			});
 		}
+		const user = users.find((u) => u._id.toString() === userID);
+		const friend = users.find((u) => u._id.toString() === friendID);
 
 		const friendIndex = user.friends.indexOf(friendID);
-		if (friendIndex === -1) {
+		const userIndex = friend.friends.indexOf(userID);
+		if (friendIndex === -1 || userIndex === -1) {
 			return res.status(400).json({
 				success: false,
 				error: "friend not found",
@@ -289,17 +292,22 @@ export const deleteFriend = async (req, res) => {
 		}
 
 		user.friends.splice(friendIndex, 1);
-		await user.save();
+		friend.friends.splice(userIndex, 1);
+		await User.bulkSave([user, friend], { session });
 
+		await session.commitTransaction();
 		return res.status(200).json({
 			success: true,
 			message: "friend deleted successfully",
 			data: user.friends,
 		});
 	} catch (error) {
+		await session.abortTransaction();
 		return res.status(500).json({
 			success: false,
 			error: error.message,
 		});
+	} finally {
+		await session.endSession();
 	}
 };
