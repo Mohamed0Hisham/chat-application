a5196b05b6ae61d8cffdd460eb2d819fa20407a4
mohamed hisham mahrous <156300477+Mohamed0Hisham@users.noreply.github.com>
2025-02-21 21:21:23 +0200

    add: send message controller

--- backend/src/messages/controllers.js
+++ backend/src/messages/controllers.js
@@ -1,5 +1,46 @@
+import Chat from "../conversations/models.js";
+import Msg from "./models.js";
+import validator from "validator";
+
 export const sendMsg = async (req, res) => {
 	try {
+		const { chatID, senderID, content } = req.body;
+		if (!validator.isMongoId(chatID) || !validator.isMongoId(senderID)) {
+			return res.status(400).json({
+				success: false,
+				message: "invalid ID sent",
+			});
+		}
+
+		if (!content) {
+			return res.status(400).json({
+				success: false,
+				message: "can't send empty message",
+			});
+		}
+
+		const chat = await Chat.findById(chatID);
+		if (!chat) {
+			return res.status(404).json({
+				success: false,
+				message: "no such chat exist",
+			});
+		}
+
+		const msg = await Msg.create({
+			senderID,
+			receiverID: chat.participants,
+			content,
+		});
+
+		chat.messages.push(msg._id);
+		await chat.save();
+
+		return res.status(201).json({
+			success: true,
+			message: "message sent",
+			data: msg,
+		});
 	} catch (error) {
 		return res.status(500).json({
 			success: false,
