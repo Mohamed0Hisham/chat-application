f375f18ed5a0c14ec4e38c71648d39e96f255454
mohamed hisham mahrous <156300477+Mohamed0Hisham@users.noreply.github.com>
2025-02-22 10:20:01 +0200

    add: authentication protection middleware

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -16,3 +16,26 @@ export const generateRefreshToken = async (user) => {
 	);
 	return token;
 };
+export const isAuthenticated = async (req, res, next) => {
+	const token = req.headers.authorization?.split(" ")[1];
+	if (!token) {
+		return res.status(401).json({
+			success: false,
+			message: "missing token within request header",
+		});
+	}
+
+	try {
+		const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+		req.user = decoded;
+		return next();
+	} catch (error) {
+		return res.status(401).json({
+			success: false,
+			message:
+				error.name === "TokenExpiredError"
+					? "Token expired"
+					: "Invalid token",
+		});
+	}
+};
