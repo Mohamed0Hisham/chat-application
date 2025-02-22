9965049748dbdc8539e56b9e74be9b7f22800f2f
mohamed hisham mahrous <156300477+Mohamed0Hisham@users.noreply.github.com>
2025-02-22 11:29:34 +0200

    add: refreshToken handler

---
+++ backend/utils/refreshToken.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import { generateAccessToken } from "./auth.js";
+export const refreshToken = (req, res) => {
+	const refreshToken = req.cookies.refreshToken;
+
+	if (!refreshToken)
+		return res
+			.status(401)
+			.json({ success: false, message: "Unauthorized" });
+
+	try {
+		const decoded = jwt.verify(
+			refreshToken,
+			process.env.JWT_REFRESH_SECRET
+		);
+
+		const newAccessToken = generateAccessToken(decoded);
+		return res.json({ accessToken: newAccessToken });
+	} catch (error) {
+		return res.status(403).json({ success: false, message: "forbidden" });
+	}
+};
