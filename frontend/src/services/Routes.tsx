cf4f0a66cceb7e9d4c4c58d346bf110b5d11cc6e
mohamed hisham mahrous <156300477+Mohamed0Hisham@users.noreply.github.com>
2025-03-04 15:21:51 +0200

    feat: index route and dashboard route

--- frontend/src/services/Routes.tsx
+++ frontend/src/services/Routes.tsx
@@ -3,19 +3,38 @@ import Register from "../pages/Auth/Register";
 import App from "../App";
 import NotFound from "../pages/error/404";
 import Login from "../pages/Auth/Login";
+import ProtectedRoute from "./ProtectedRoute";
+import Dashboard from "../components/chat/Dashboard";
 
 const routes = createBrowserRouter([
 	{
 		path: "/",
 		element: <App />,
+		errorElement: <NotFound />,
 		children: [
+			{
+				index: true, // Default route
+				element: (
+					<ProtectedRoute>
+						<Dashboard />
+					</ProtectedRoute>
+				),
+			},
 			{
 				path: "register",
 				element: <Register />,
 			},
 			{
-				path:"login",
-				element:<Login/>
+				path: "login",
+				element: <Login />,
+			},
+			{
+				path: "dashboard",
+				element: (
+					<ProtectedRoute>
+						<Dashboard />
+					</ProtectedRoute>
+				),
 			},
 			{
 				path: "*",
