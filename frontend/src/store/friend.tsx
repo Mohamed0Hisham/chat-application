6b864f52ce702288bd453c3208ba6edc7d3af6e3
mohamed hisham mahrous <156300477+Mohamed0Hisham@users.noreply.github.com>
2025-03-09 00:15:00 +0200

    add: initiate friends store

--- frontend/src/store/friend.tsx
+++ frontend/src/store/friend.tsx
@@ -1,23 +1,47 @@
 import { create } from "zustand";
 import api from "../services/api";
 
+type Friend = { _id: string; fullname: string; isOnline: boolean };
 interface FriendState {
-	fullname?: string;
-	isOnline: boolean;
+	friend?: Friend;
+	friends?: Friend[];
 	isLoading: boolean;
-
-    getFriend: ()=>Promise<void>
+	getFriend: () => Promise<void>;
+	getFriends: () => Promise<void>;
 }
 const useFriendStore = create<FriendState>((set) => ({
-	fullname: undefined,
-	isOnline: false,
+	friend: undefined,
+	friends: undefined,
 	isLoading: false,
 
-    getFriend: async () => {
-        set({isLoading:true})
-        await api.get("/friend")
-    }
+	getFriend: async () => {
+		set({ isLoading: true });
+		try {
+			const friend: Friend = await api.get(
+				"/users/:userID/friends/:friendID"
+			);
+			set({
+				friend,
+				isLoading: true,
+			});
+		} catch (error) {
+			console.error(error);
+			set({ friend: undefined, friends: undefined, isLoading: false });
+		}
+	},
+	getFriends: async () => {
+		set({ isLoading: true });
+		try {
+			const friends: Friend[] = await api.get("/users/:userID/friends");
+			set({
+				friends,
+				isLoading: true,
+			});
+		} catch (error) {
+			console.error(error);
+			set({ friend: undefined, friends: undefined, isLoading: false });
+		}
+	},
 }));
-
 
 export default useFriendStore;
