7ab7e3dec24a8a847c163f3301a4b77d9842b54f
mohamed hisham mahrous <156300477+Mohamed0Hisham@users.noreply.github.com>
2025-03-08 15:21:32 +0200

    add: styles and responsivness for dashboard

--- frontend/src/pages/Chat/Conversation.tsx
+++ frontend/src/pages/Chat/Conversation.tsx
@@ -1,15 +1,29 @@
 // Chat.tsx
-import  { useState,FC } from "react";
+import { useState, FC } from "react";
 import { SenderBubble } from "../../components/chat/SenderBubble";
 import { ReceiverBubble } from "../../components/chat/ReceiverBubble";
 import styles from "./Conversation.module.css";
 import { Paperclip, SendHorizonal, Smile } from "lucide-react";
 import { MessageInput } from "../../components/chat/MessageInput";
 
+const src = "https://img.icons8.com/?size=100&id=11730&format=png&color=000000";
 const Chat: FC = () => {
 	const [message, setMessage] = useState("");
 	return (
 		<div className={styles.conv}>
+			<div className={styles.friend}>
+				<div className={styles.avatar}>
+					<img src={src} alt="avatar" />
+				</div>
+				<div className={styles.description}>
+					<p>your friend</p>
+					{true ? (
+						<div className={styles.green}></div>
+					) : (
+						<div className={styles.red}></div>
+					)}
+				</div>
+			</div>
 			<div className={styles.chatContainer}>
 				<ReceiverBubble
 					message="Hey! How's it going?"
@@ -20,45 +34,41 @@ const Chat: FC = () => {
 					timestamp="10:31 AM"
 				/>
 				<ReceiverBubble
-					message="Doing great! Want to grab coffee later?"
-					timestamp="10:32 AM"
+					message="Hey! How's it going?"
+					timestamp="10:30 AM"
 				/>
 				<SenderBubble
-					message="Sure, sounds perfect!"
-					timestamp="10:33 AM"
+					message="Pretty good, thanks! How about you?"
+					timestamp="10:31 AM"
 				/>
 				<ReceiverBubble
-					message="Doing great! Want to grab coffee later?"
-					timestamp="10:32 AM"
+					message="Hey! How's it going?"
+					timestamp="10:30 AM"
 				/>
 				<SenderBubble
-					message="Sure, sounds perfect!"
-					timestamp="10:33 AM"
+					message="Pretty good, thanks! How about you?"
+					timestamp="10:31 AM"
 				/>
 				<ReceiverBubble
-					message="Doing great! Want to grab coffee later?"
-					timestamp="10:32 AM"
+					message="Hey! How's it going?"
+					timestamp="10:30 AM"
 				/>
 				<SenderBubble
-					message="Sure, sounds perfect!"
-					timestamp="10:33 AM"
+					message="Pretty good, thanks! How about you?"
+					timestamp="10:31 AM"
 				/>
 				<ReceiverBubble
-					message="Doing great! Want to grab coffee later?"
-					timestamp="10:32 AM"
+					message="Hey! How's it going?"
+					timestamp="10:30 AM"
 				/>
 				<SenderBubble
-					message="Sure, sounds perfect!"
-					timestamp="10:33 AM"
-				/>
-				<ReceiverBubble
-					message="Doing great! Want to grab coffee later?"
-					timestamp="10:32 AM"
+					message="Pretty good, thanks! How about you?"
+					timestamp="10:31 AM"
 				/>
 			</div>
 			<div className={styles.msgIn}>
 				<span className={styles.attach}>
-					<Paperclip className={styles.attachIcon}/>
+					<Paperclip className={styles.attachIcon} />
 				</span>
 				<MessageInput
 					value={message}
@@ -66,10 +76,10 @@ const Chat: FC = () => {
 					placeholder="Type your message here..."
 				/>
 				<span className={styles.smile}>
-					<Smile className={styles.smileIcon}/>
+					<Smile className={styles.smileIcon} />
 				</span>
 				<span className={styles.send}>
-					<SendHorizonal className={styles.sendIcon}/>
+					<SendHorizonal className={styles.sendIcon} />
 				</span>
 			</div>
 		</div>
