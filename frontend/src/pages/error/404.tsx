7840b0190e86644f1be63a7c016b972c2ea4cc03
mohamed hisham mahrous <156300477+Mohamed0Hisham@users.noreply.github.com>
2025-03-13 12:01:42 +0200

    fix: bug of global css source at 404

--- frontend/src/pages/error/404.tsx
+++ frontend/src/pages/error/404.tsx
@@ -1,11 +1,13 @@
-import "./404.css";
+import styles from "./404.module.css";
 const NotFound = () => {
 	return (
-		<section>
-			<h1>404</h1>
+		<section className={styles.containerE}>
+			<h1 className={styles.h1}>404</h1>
 			<div>
-				<h2>Page not found!</h2>
-				<p>there is no page exist for the required path</p>
+				<h2 className={styles.h2}>Page not found!</h2>
+				<p className={styles.p}>
+					there is no page exist for the required path
+				</p>
 			</div>
 		</section>
 	);
