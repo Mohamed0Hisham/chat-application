6430c896bb797aecc64d8feb6c4eb8ca9fb8e087
mohamed hisham mahrous <156300477+Mohamed0Hisham@users.noreply.github.com>
2025-03-17 22:22:55 +0200

    feat: socket.io initiated @ chat Component

--- frontend/src/pages/Chat/chat.tsx
+++ frontend/src/pages/Chat/chat.tsx
@@ -1,9 +1,42 @@
+import { useEffect } from "react";
 import Conversation from "../../components/friends/Conversation";
 import useFriendStore from "../../store/friend";
 import styles from "./chat.module.css";
+import { io } from "socket.io-client";
+import useAuthStore from "../../store/Auth-Store";
 
 const Chat = () => {
 	const { friend } = useFriendStore();
+	const { accessToken } = useAuthStore();
+
+	useEffect(() => {
+		const socket = io("http://localhost:7536", {
+			auth: {
+				token: accessToken,
+			},
+		});
+
+		socket.on("connect", () => {
+			console.log("Connected to server");
+		});
+
+		socket.on("receiveMessage", (message) => {
+			console.log("Received message:", message);
+		});
+
+		socket.on("disconnect", () => {
+			console.log("Disconnected from server");
+		});
+
+		socket.on("connect_error", (error) => {
+			console.error("Connection error:", error);
+		});
+
+		return () => {
+			socket.disconnect();
+		};
+	}, [accessToken]);
+
 	return (
 		<main className={styles.chat}>
 			{friend ? (
